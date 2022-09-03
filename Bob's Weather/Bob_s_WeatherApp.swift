31a774fd5a4703af705e90ea95c9b99e5281d4af
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-09-03 02:15:04 +0200

    Initial Commit

---
+++ Bob's Weather/Bob_s_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bob_s_WeatherApp.swift
+//  Bob's Weather
+//
+//  Created by user222661 on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Bob_s_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bob's Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bob's Weather
+//
+//  Created by user222661 on 9/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
